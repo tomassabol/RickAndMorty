870bd9189b0c19ac2fcfd48cbc906f123f476465
Tomáš Sabol <tomassabol2@gmail.com>
2022-12-27 23:13:47 +0100

    api communication layer wip

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,5 +9,53 @@ import Foundation
 
 /// object that represents single API call
 final class RMRequest {
+    // Base URL
+    // Endpoint
     
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    /// constructive url for api requests in a string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public let httpMethod = "GET"
+    
+    init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.queryParameters = queryParameters
+        self.pathComponents = pathComponents
+    }
 }
--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -20,7 +20,11 @@ final class RMService {
     ///  - Parameters
     ///     - request: Request instance
     ///     - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void ) {
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
         
     }
     
